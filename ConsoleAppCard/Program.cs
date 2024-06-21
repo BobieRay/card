e9d646f43d5a8b6e9a031a94a320e781b98650d1
S-JOBI23-051\student <231123@st.yoshida-g.ac.jp>
2024-06-21 11:46:26 +0900

    1～4の数字を入力するフォームを4つ用意する。 #1

---
+++ ConsoleAppCard/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 配列
+            int[] card = new int[4];
+
+            // 配列分繰り返す
+            for (int i = 0; i < card.Length; i++)
+            {
+                Console.WriteLine("{0}番目のカードを入力してください。 >",i+1);
+                string inputStr = Console.ReadLine();
+                int.TryParse(inputStr, out card[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
